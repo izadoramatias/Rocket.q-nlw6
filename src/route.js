deadc933e21922b79e1687eb22e72329a76581d6
izadora matias <contato.izadoramatias@gmail.com>
2022-04-23 19:05:16 -0300

    dinamic questions and actions

--- src/route.js
+++ src/route.js
@@ -6,10 +6,11 @@ const roomController = require('./controllers/room-controller');
 route.get('/', (req, res) => res.render('index', {page: 'enter-room'}));
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
+route.post('/create-room', roomController.create)
 route.get('/room/:room', roomController.open)
 
+route.post("/question/create/:room", questionController.create)
 // formato que o formulário da modal passará a informação
 route.post('/question/:room/:question/:action', questionController.index)
-route.post('/create-room', roomController.create)
 
 module.exports = route
